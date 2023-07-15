5b3c9a75ad8d4adab4f8613d99b9d0132379548e
Karina <karina.ispembetova05@.gmail.com>
2023-07-16 00:06:09 +0600

    j

---
+++ 50/Program.cs
@@ -0,0 +1,47 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет   */
+Console.WriteLine("введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+int [,] numbers = new int [10,10];
+FillArrayRandomNumbers(numbers);
+
+
+if (rows > numbers.GetLength(0) || columns > numbers.GetLength(1)){
+    Console.WriteLine("такого элемента нет");
+}
+else{
+    Console.WriteLine($"значение элемента {rows} строки и {columns} столбца равно {numbers[rows-1,columns-1]}");
+}
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToInt32(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+PrintArray(numbers);
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
